71e021315ed31a23165db13a557fedd07622fe69
normyp <pm.normington@gmail.com>
2020-07-02 11:14:07 +0100

    Added bullets into the game

--- settings.py
+++ settings.py
@@ -5,3 +5,8 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (0, 100, 0)
         self.ship_speed = 1.5
+
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (60,60,60)
